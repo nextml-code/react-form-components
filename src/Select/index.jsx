da2b04d56bbb37819a2dd6550340ba3a7abff961
Filip Johansson <filip.johansson@protonmail.com>
2020-11-19 13:04:22 +0100

    refactor: better state management and some default value fix

--- src/Select/index.jsx
+++ src/Select/index.jsx
@@ -1,24 +1,102 @@
-import React, { useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import RenderGate from "@codewell/render-gate";
 import Options from "./Options";
 import Wrapper from "./Wrapper";
 import Input from "./Input";
+import superSearch from "@codewell/super-search";
+import isDefined from "@codewell/is-defined";
 
-const Select = ({ options, renderAs, onSelect, defaultIndex = 0 }) => {
-  const [filter, setFilter] = useState(renderAs(options[defaultIndex]));
-  const [expanded, setExpanded] = useState(false);
+const initialState = {
+  filter: "",
+  expanded: false,
+  selectedOption: -1,
+  showFilterInput: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_EXPANDED": {
+      return {
+        ...state,
+        expanded: action.payload,
+      };
+    }
+
+    case "SET_FILTER": {
+      return {
+        ...state,
+        filter: action.payload,
+      };
+    }
+
+    case "SET_SELECTED_OPTION": {
+      return {
+        ...state,
+        selectedOption: action.payload,
+      };
+    }
+
+    case "SELECT_OPTION": {
+      return {
+        ...state,
+        expanded: false,
+        showFilterInput: false,
+        filter: "",
+        selectedOption: action.payload,
+      };
+    }
+
+    case "RESET_FILTER": {
+      return {
+        ...state,
+        filter: "",
+        showFilterInput: true,
+        expanded: false,
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
+
+const renderSelectedOption = (state, options, renderAs) => {
+  if (state.selectedOption === -1) {
+    return "";
+  }
+
+  return renderAs(options[state.selectedOption]);
+};
+
+const filterOptions = (options, filter) =>
+  options.filter((option) => superSearch(filter, option).numberOfMatches > 0);
+
+const Select = ({ options, renderAs, onSelect, defaultIndex = -1 }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: "SET_SELECTED_OPTION", payload: defaultIndex });
+  }, [defaultIndex]);
 
   return (
     <Wrapper>
-      <Input setExpanded={setExpanded} setFilter={setFilter} filter={filter} />
-      <RenderGate condition={expanded}>
+      <Input
+        state={state}
+        dispatch={dispatch}
+        renderedOption={renderSelectedOption(state, options, renderAs)}
+      />
+
+      <RenderGate condition={state.expanded}>
         <Options
-          options={options}
-          filter={filter}
+          state={state}
+          dispatch={dispatch}
+          options={filterOptions(
+            options.map((option, index) => ({ ...option, optionIndex: index })),
+            state.filter,
+          )}
           renderAs={renderAs}
           onSelect={onSelect}
-          setExpanded={setExpanded}
-          setFilter={setFilter}
         />
       </RenderGate>
     </Wrapper>
