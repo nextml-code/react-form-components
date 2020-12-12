2df8ce9aa3740e0bd6f71407dcfac9ba1701dff3
Filip Johansson <filip.johansson@protonmail.com>
2020-12-12 19:44:30 +0100

    build: format code

--- src/Checkbox/index.jsx
+++ src/Checkbox/index.jsx
@@ -1,23 +1,22 @@
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, {useState} from 'react';
-import styled from 'styled-components';
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
-display: flex;
-align-items: center;
-`
-
+  display: flex;
+  align-items: center;
+`;
 
 const StyledCheckbox = styled.button`
-display: flex;
-justify-content: center;
-align-items: center;
-border-radius: var(--aiwizo-application-border-radius-primary);
-border: 1px solid var(--aiwizo-application-primary-border-grey);
-width: 18px;
-height: 18px;
-background-color: #ffffff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: var(--aiwizo-application-border-radius-primary);
+  border: 1px solid var(--aiwizo-application-primary-border-grey);
+  width: 18px;
+  height: 18px;
+  background-color: #ffffff;
 `;
 
 const StyledLabel = styled.span`
@@ -25,28 +24,32 @@ const StyledLabel = styled.span`
   font-family: var(--aiwizo-application-default-font);
   margin-left: var(--aiwizo-application-spacing-small);
   color: var(--aiwizo-application-grey-dark);
-`
+`;
 
 const handleEvent = (onChange, setChecked, value, checked) => () => {
-  onChange({value, checked})
-  setChecked(!checked)
-}
+  onChange({ value, checked });
+  setChecked(!checked);
+};
 
-const Checkbox = ({onChange, value, label}) => {
-  const [checked, setChecked] = useState(false)
-
+const Checkbox = ({ onChange, value, label }) => {
+  const [checked, setChecked] = useState(false);
 
   return (
     <Wrapper>
-
-    <StyledCheckbox type="button" onClick={handleEvent(onChange, setChecked, value, checked)}>
-
-{checked ? <FontAwesomeIcon icon={faCheck} style={{color: 'var(--aiwizo-application-green)'}}/> : null}
-
-    </StyledCheckbox>
-    <StyledLabel>{label}</StyledLabel>
+      <StyledCheckbox
+        type="button"
+        onClick={handleEvent(onChange, setChecked, value, checked)}
+      >
+        {checked ? (
+          <FontAwesomeIcon
+            icon={faCheck}
+            style={{ color: "var(--aiwizo-application-green)" }}
+          />
+        ) : null}
+      </StyledCheckbox>
+      <StyledLabel>{label}</StyledLabel>
     </Wrapper>
-  )
+  );
 };
 
 export default Checkbox;
