3ec3b6fdfb0fa23893eb425a3cce4aafc5558194
Filip Johansson <filip.johansson@protonmail.com>
2020-12-14 13:35:38 +0100

    feature: input component

---
+++ src/Input/Label.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Label = styled.label`
+  font-size: var(--aiwizo-application-font-size-regular);
+  font-family: var(--aiwizo-application-default-font);
+  display: flex;
+  flex-direction-column;
+  margin-bottom: var(--aiwizo-application-spacing-mini);
+  font-weight: 600;
+  color: var(--aiwizo-application-grey);
+`;
+
+export default Label;
