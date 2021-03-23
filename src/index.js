a0617228792a5aec3fb355d5d0c151b622a5a8d3
Filip Johansson <filip.johansson@protonmail.com>
2021-03-23 13:21:19 +0100

    feature!: use @aiwizo/application-styles

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import Select from "./Select/index.jsx";
 import Checkbox from "./Checkbox/index.jsx";
 import Button from "./Button/index.js";
 import Input from "./Input/index.jsx";
+import "@aiwizo/application-styles";
 
 export { Select, Checkbox, Button, Input };
